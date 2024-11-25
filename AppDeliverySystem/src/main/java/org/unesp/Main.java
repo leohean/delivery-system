045c4f9a9188ce4ee6b886c1136af61061863430
leohean <leohean_cardoso@hotmail.com>
2024-11-25 19:44:10 -0300

    Criado a estrutura de lista circular para os pontos de redistribuição

--- AppDeliverySystem/src/main/java/org/unesp/Main.java
+++ AppDeliverySystem/src/main/java/org/unesp/Main.java
@@ -1,9 +1,31 @@
 package org.unesp;
 
 import org.unesp.builders.VehicleBuilder;
+import org.unesp.dataStructure.CircularList;
+import org.unesp.entities.Delivery;
+import org.unesp.entities.Redistributor;
+import org.unesp.entities.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
         VehicleBuilder vehicleBuilder = new VehicleBuilder();
+
+        CircularList cl = new CircularList();
+
+        Vehicle v1 = new Vehicle(1, 10, null);
+        Vehicle v2 = new Vehicle(2, 10, null);
+        Vehicle v3 = new Vehicle(3, 10, null);
+        //Adds data to the list
+        Redistributor r1 = new Redistributor(null, v1);
+        Redistributor r2 = new Redistributor(null, v2);
+        Redistributor r3 = new Redistributor(null, v3);
+
+        cl.add(r1);
+        cl.add(r2);
+        cl.add(r3);
+        //Displays all the nodes present in the list
+        cl.display();
+
+        System.out.println(cl.getNextRedistributor(r3));
     }
 }
---
+++ AppDeliverySystem/src/main/java/org/unesp/dataStructure/CircularList.java
@@ -0,0 +1,60 @@
+package org.unesp.dataStructure;
+
+import org.unesp.entities.Redistributor;
+
+public class CircularList {
+
+    public class Node{
+        Redistributor redistributor;
+        Node next;
+        public Node(Redistributor redistributor) {
+            this.redistributor = redistributor;
+        }
+    }
+
+    //Declaring head and tail pointer as null.
+    public Node head = null;
+    public Node tail = null;
+
+    public void add(Redistributor redistributor){
+
+        Node newNode = new Node(redistributor);
+
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+            newNode.next = head;
+        }
+        else {
+            //tail will point to new node.
+            tail.next = newNode;
+            //New node will become new tail.
+            tail = newNode;
+            //Since, it is circular linked list tail will point to head.
+            tail.next = head;
+        }
+    }
+
+    public Redistributor getNextRedistributor(Redistributor redistributor){
+        Node current = head;
+        do{
+            current = current.next;
+        }while(current.redistributor != redistributor);
+        return current.next.redistributor;
+    }
+
+    public void display() {
+        Node current = head;
+        if(head == null) {
+            System.out.println("List is empty");
+        }
+        else {
+            System.out.println("Nodes of the circular linked list: ");
+            do{
+                System.out.println(" "+ current.redistributor);
+                current = current.next;
+            }while(current != head);
+            System.out.println();
+        }
+    }
+}
