e92e275a0f2eb0fd3136beab31a1b8491f092d93
mateusflores <mateusgf.42@gmail.com>
2024-11-26 10:50:44 -0300

    add id e metodo toString() para classe Redistributor

--- AppDeliverySystem/src/main/java/org/unesp/entities/Redistributor.java
+++ AppDeliverySystem/src/main/java/org/unesp/entities/Redistributor.java
@@ -3,12 +3,24 @@ package org.unesp.entities;
 import java.util.List;
 
 public class Redistributor implements Runnable{
+    private static int counter = 0;
+    private String id;
     private List<Delivery> listOfDeliveries;
     private Vehicle currentVehicle;
 
     public Redistributor(List<Delivery> listOfDeliveries, Vehicle currentVehicle) {
         this.listOfDeliveries = listOfDeliveries;
         this.currentVehicle = currentVehicle;
+        setId(counter);
+        counter++;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = "R" + id;
     }
 
     public List<Delivery> getListOfDeliveries() {
@@ -25,6 +37,13 @@ public class Redistributor implements Runnable{
 
     public void setCurrentVehicle(Vehicle currentVehicle) {
         this.currentVehicle = currentVehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "Redistributor{" +
+                "id=" + id +
+                '}';
     }
 
     @Override
